868b31c2d5f681bca359d603c1028ed350a29781
João <joaoaugusto311016@gmail.com>
2023-05-23 15:17:54 -0300

    23-05

--- src/SmartTv.java
+++
@@ -1,7 +0,0 @@
-public class SmartTv {
-    public static void Main(String[] args){
-        boolean ligado = true;
-        int canal = 1;
-        int volume = 25;
-    }
-}
--- src/Usuario.java
+++
@@ -1,7 +0,0 @@
-public class Usuario {
-    public static void Main(String[] args) throws Exception{
-        String nome = "João Augusto";
-        System.out.println(nome);
-    }
-}
-
