1c7ded0450a54db679fd35a5566d981cf460bd5a
João <joaoaugusto311016@gmail.com>
2023-05-23 16:11:23 -0300

    23-05

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[]  args){
-      System.out.println("Alô Mundo!");
+      System.out.println("Esse é o screen math!");
+        System.out.println("Animes: Top animes: Naruto");
+
+        int ano = 2023;
+        System.out.println("Ano de lançamento: " + ano);
+
+        double media = (7.7 + 8.2 + 5.9) / 3;
+        System.out.println(media);
+
+        String descricao;
+        descricao = "Melhor anime que já assisti em toda minha vida";
+        System.out.println(descricao);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }
